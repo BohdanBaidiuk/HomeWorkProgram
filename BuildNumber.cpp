4e9f913ba1c8901d5c5d72ffbb22c2a3951229f6
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-06-25 05:54:53 +0300

    Update BuildNumber.cpp
    Поменял тип данных numA,numB,numC - с Long на int.
    Диапазон numeric_limits - зделал как константу, поменял тип данных на unsigned short и диапазон задал unsigned short.
    Распределил ввод numA,numB,numC - перед каждой ихней проверкой  if.
    if (newNum % 3 == 0) - переписал на тернарный оператор.
    Структурировал код.

--- BuildNumber.cpp
+++ BuildNumber.cpp
@@ -3,35 +3,30 @@
 
 int main(){
 
-	unsigned long long  numA,numB,numC;
-	unsigned long long max = std::numeric_limits<unsigned long>::max();
-	unsigned long long min = std::numeric_limits<unsigned long>::min();
-	std::cout << "Enter three numbers A, B, C \n";
-	std::cin >> numA >> numB >> numC;
-	unsigned long long newNum;
-	if (numA>= min && numA <= max) {
-		if (numB >= min && numB <= max) {
-			if (numC >= min && numC <= max) {
-				newNum = numA * 10000 + numB * 1000 + numC * 100;
-
-				if (newNum % 3 == 0) {
-					std::cout << newNum << " = This number is divided by 3 ";
-				}
-				else {
-					std::cout << newNum << " = This number is not divisible by 3";
-				}
-        
-			}
-			else {
-				std::cout << "Number C not range ";
-			}
-		}
-		else {
-			std::cout << "Number B not range ";
-		}
-	}
-	else {
-		std::cout << "Number A not range ";
-	}
-	return 0;
+  unsigned int numA,numB,numC;
+  const unsigned short MAX max = std::numeric_limits<unsigned short>::max();
+  const unsigned short MIN = std::numeric_limits<unsigned short>::min();
+  std::cout <<"If you want to build a new number\n"<<"please enter three numbers A, B, C \n";
+  std::cin >> numA;
+  if (numA>= min && numA <= max) {
+    std::cin >> numB;
+    if (numB >= min && numB <= max) {
+      std::cin >> numC;
+      if (numC >= min && numC <= max) {
+        unsigned long newNum;
+        newNum = numA * 10000 + numB * 1000 + numC * 100;
+        newNum % 3 == 0 ? std::cout << newNum << " = This number is divided by 3 " : std::cout << newNum << " = This number is not divisible by 3";
+       }	      
+      else {
+        std::cout << "Number C not range ";
+      }
+    }
+    else {
+      std::cout << "Number B not range ";
+    }
+  }
+  else {
+    std::cout << "Number A not range ";
+  }
+  return 0;
 }
