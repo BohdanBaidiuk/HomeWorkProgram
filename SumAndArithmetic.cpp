8aae393595bf01af407ddf41fe541f5f3880c786
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-06-29 12:08:36 +0300

    Update SumAndArithmetic.cpp

--- SumAndArithmetic.cpp
+++ SumAndArithmetic.cpp
@@ -3,28 +3,36 @@
 
 
 int main() {
-	unsigned number;
-	unsigned max = std::numeric_limits<unsigned short>::max();
-	unsigned min = std::numeric_limits<unsigned short>::min();
-	std::cout << "Input number - from "<< min <<" to "<< max <<"\n";
-	std::cin >> number;
-	if (number >= min && number <= max) {
-		std::cout << "\n";
-		std::cout << "Sum of number " << number << " ===\t";
-		long double sum = 0;
-		int count = 0;
-		for (; number != 0;) {
-			sum += number % 10;
-			number /= 10;
-			count++;
-		}
-		std::cout << sum << "\n\n";
-		std::cout << "Arithmetic average ===\t";
-		auto mult = sum / count;
-		std::cout << mult << "\n";
-	}
-	else {
-		std::cout << "Not range";
-	}
-	return 0;
+  unsigned number;
+  const unsigned short MAX = std::numeric_limits<unsigned short>::max();
+  const unsigned short MIN = std::numeric_limits<unsigned short>::min();
+
+  std::cout <<"Calculate the sum and arithmetic mean of numbers in the number \n"
+  "Input number - from "<< MIN <<" to "<< MAX <<"\n";
+  "But number should not be 0 \n";
+  std::cin >> number;
+  if(number!=0){
+    if (number >= MIN && number <= MAX) {
+      std::cout << "\n";
+      std::cout << "Sum of number " << number << " ===\t";
+      long sum = 0;
+      unsigned count = 0;
+      while (number != 0) {
+        sum += number % 10;
+        number /= 10;
+        count++;
+      }
+      std::cout << sum << "\n\n";
+      std::cout << "Arithmetic average ===\t";
+      auto division = sum / (count + 0.f);
+      std::cout << division << "\n";
+    }
+    else {
+      std::cout << "Number range from "<<MIN<<" to "<<MAX;
+    }
+  }
+  else{
+      std::cout << "Number should not be 0;
+  }
+  return 0;
 }
