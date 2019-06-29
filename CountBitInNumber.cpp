33fbf4fe2fa52f3436f3c19d200967623cddf625
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-06-29 11:30:53 +0300

    Update CountBitInNumber.cpp
    Убрал диапазон . Структурировал код. Поменял for на while.

--- CountBitInNumber.cpp
+++ CountBitInNumber.cpp
@@ -2,23 +2,16 @@
 
 int main() {
 
-	unsigned  number,bit;
-	unsigned maxBit = 255;
-	unsigned minBit = 1;
-	std::cout << "Inpute number from "<< minBit<<" to "<< maxBit <<"\n";
-	std::cin >> bit;
-	number = bit;
-	if (bit >= minBit && bit <= maxBit) {
-		int count = 0;
-		for (; bit != 0;) {
-			count += bit & 1;
-			bit >>= 1;
-		}
-		std::cout << "In number = " << number << " is a bit = " << count;
-	}
-	else {
-		std::cout << "Not range";
-	}
-	return 0;
+  unsigned  number;
+  unsigned bit;
+  std::cout << "Inpute number to find out how many bits in the number\n";
+  std::cin >> bit;
+  number = bit;
+  unsigned count = 0;
+  while (bit != 0) {
+    count += bit & 1;
+    bit >>= 1;
+  }
+  std::cout << "In number = " << number << " is a bit = " << count;
+  return 0;
 }
-
