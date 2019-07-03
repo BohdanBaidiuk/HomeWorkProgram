e3fc924903803cd97381a30dcf145bed4a50f236
BohdanBaidiuk <51362296+BohdanBaidiuk@users.noreply.github.com>
2019-07-03 11:23:33 +0300

    Update Home_Work_4.cpp

--- Home_Work_4.cpp
+++ Home_Work_4.cpp
@@ -1,91 +1,105 @@
 #include <iostream>
 
-void FindDigest(char arg_symbol[]) {
-  int size = 0;
-  for (; arg_symbol[size] != '\0'; size++);
-  for (int i = 0 ; i < size ; i++) {
-    if (arg_symbol[i] >= '0' && arg_symbol[i] <= '9') {
-     std::cout << arg_symbol[i];
+void FindDiget(char *arg_symbol,char *arg_diget, int arg_length) {
+  bool dig_check = false;
+  int count = 0;
+  for (int i = 0; i < arg_length; i++){
+    if (arg_symbol[i] >= '0' && arg_symbol[i] <= '9'){
+      arg_diget[count++] = arg_symbol[i];
+      dig_check = true;
     }
   }
-}
+  if (dig_check){
+    arg_diget[count] = 0;
+    std::cout << "Is Dig = " << arg_diget <<"\n";
+  }
+  else
+    std::cout << "No digits in string \n";	
+  }
 
-void Tolower(char arg_symbol[]) {
-  char temp[50];
-  int size = 0;
-  for (; arg_symbol[size] != '\0'; size++);
-  for (int i = 0 ; i <= size ; i++) {
+void Tolower(char *arg_symbol, int arg_length) {
+  for (int i = 0; i <= arg_length; i++) {
     if (arg_symbol[i] >= 'A' && arg_symbol[i] <= 'Z') {
-      temp[i] = arg_symbol[i] + 32;
-      std::cout << temp[i];
+      const unsigned short MAGIK_NUM = 32;
+      int temp = arg_symbol[i] + MAGIK_NUM;
+      std::cout << (char)temp;
     }
     else {
-      std::cout<<arg_symbol[i];
+      std::cout << arg_symbol[i];
     }
-  }
-	
+  }	
 }
 
-void ToUpper(char arg_symbol[]) {
-  char temp[50];
-  int size = 0;
-  for (; arg_symbol[size] != '\0'; size++);
-  for (int i = 0 ; i <= size ; i++) {
+void ToUpper(char *arg_symbol,int arg_length) {
+  for (int i = 0 ; i <= arg_length; i++) {
     if (arg_symbol[i] >= 'a' && arg_symbol[i] <= 'z') {
-      temp[i] = arg_symbol[i] - 32;
-      std::cout << temp[i];
+      const unsigned short MAGIK_NUM = 32;
+      int temp = arg_symbol[i] - MAGIK_NUM;
+      std::cout << (char)temp;;
     }
     else {
       std::cout<<arg_symbol[i];
     }
   }
 }
-void ReversString(char arg_str[]) {
-  char temp;
-  int size = 0;
-  for ( ; arg_str[size] != '\0'; size++);
-  size--;
-  for (int i=0 ; i <= size ; i++){
-    temp = arg_str[i];
-    arg_str[i] = arg_str[size];
-    arg_str[size] = temp;
-    size--;
+
+void ReversString(char *arg_str,int arg_length) {
+  for (int i = 0, j = arg_length - 1; i < j; i++, j--) {
+    int temp = arg_str[i];
+    arg_str[i] = arg_str[j];
+    arg_str[j] = temp;
   }
 }
 
+enum ChoiseFunc {
+	FIND = 1,
+	UPPER = 2,
+	LOWER = 3,
+	REVERS = 4
+};
 
 
 int main() {
-
-  const unsigned size = 50;
-  char symbol[size];
-  std::cout << "Enter symbols = ";
+  std::cout << "In this program you can = \n"
+  "-Find number in string \n" <<
+  "-Upper case conversion \n" <<
+  "-Lower case conversion \n" <<
+  "-Revers for string \n";
+  const unsigned SIZE = 100;
+  char symbol[SIZE];
+  char arrDiget[SIZE];
+  std::cout << "Enter STRING = ";
   std::cin >> symbol;
-  unsigned short choice;
-  std::cout << "Input choice = \n" <<
-    "1 - Find number in string\n" <<
-    "2 - uppercase translation\n" <<
-    "3 - lower case conversion\n" <<
-    "4 - revers for string\n";
-  std::cin >> choice;
-  switch (choice) {
-  case 1:
-    std::cout << "Is digest = ";
-    FindDigest(symbol);
-    break;
-  case 2:
-    Tolower(symbol);
-    break;
-  case 3:
-    ToUpper(symbol);
-    break;
-  case 4:
-    ReversString(symbol);
-    std::cout << symbol;
-    break;
-
-  default:
-    std::cout << "Eror";
-  break;
+  size_t length = strlen(symbol);
+  if (length <= SIZE) {
+    std::cout << "Select a function by the following commands : \n";
+    std::cout << "---1 = Find number\n"
+    << "---2 = Upper case \n"
+    << "---3 = Lower case \n"
+    << "---4 = Revers \n\n";
+    unsigned short choice;
+    std::cin >> choice;
+    switch (ChoiseFunc(choice)) {
+      case FIND:
+        FindDiget(symbol, arrDiget, length);
+        break;
+      case UPPER:
+        Tolower(symbol, length);
+        break;
+      case LOWER:
+        ToUpper(symbol, length);
+        break;
+      case REVERS:
+        ReversString(symbol, length);
+        std::cout << symbol;
+        break;
+      default:
+        std::cout << "UNDEFINED CHOICE";
+        break;
+    }
   }
+  else {
+    std::cout << "String  very long for program!!!";
+  }
+  return 0;
 }
